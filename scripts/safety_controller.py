Added PFL Limit Computation + Scaling Bugfix

scripts/safety_controller.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import numpy as np
+import os, numpy as np
 from termcolor import colored
 
 # Import ROS2 Messages
@@ -25,6 +25,7 @@ class SafetyController():
         # Robot Parameters
         self.stopping_time, self.stopping_distance = robot_parameters['stopping_time'], robot_parameters['stopping_distance']
         self.max_speed = max(robot_parameters['q_limits'])
+        self.robot_mass = robot_parameters['mass']
 
     def compute_robot_point(self, joint_states:JointState) -> Vector3:
 
@@ -61,7 +62,7 @@ class SafetyController():
         """ Compute Speed-And-Separation Monitoring ISO/TS 15066 Velocity Limit """
 
         """
-            A safety-aware kinodynamic architecture for human-robot collaboration
+            A Safety-Aware Kinodynamic Architecture for Human-Robot Collaboration
             Andrea Pupa, Mohammad Arrfou, Gildo Andreoni, Cristian Secchi
 
             https://arxiv.org/pdf/2103.01818.pdf
@@ -117,20 +118,126 @@ class SafetyController():
         # Compute ISO/TS 15066 Velocity Limit
         return (Sp - Vh * (Ts + Tr) - C - Zd - Zr) / (Ts + Tr) - (a_max * Ts**2) / (2 * (Ts + Tr))
 
-        """ Compute Power and Force Velocity Limit (PFL) """
-
     def compute_PFL_vel_lim(self, P_H:Vector3, P_R:Vector3, hr_versor:Vector3, human_vel:Vector3) -> float:
 
         """ Compute Power and Force Limiting ISO/TS 15066 Velocity Limit """
 
-        # quando scaling va quasi a zero, switcha a pfl !
-        # quando la vel scalata scende sotto la soglia del pfl switcho -> es 200mm/s -> scendo
-        # ogni loop calcolo Vpfl, Vssm e ho la v desired
-        # se Vssm < Vpfl -> v desired = Vpfl
-        # a = 1 se vrobot < vpfl
-        # bound = max(vpfl, vssm)
+        """ ISO/TS 15066 - Appendix A
 
-        pass
+            E = F^2 / 2k = A^2 * p^2 / 2k = 1/2 * μ * v_rel^2
+
+                E:     transfer energy.
+                F:     maximum contact force for specific body region (Table A.2).
+                p:     maximum contact pressure for specific body area (Table A.2).
+                k:     effective spring constant for specific body region (Table A.3).
+                A:     area of contact between robot and body region.
+                v_rel: relative speed between the robot and the human body region.
+                μ:     reduced mass of the two-body system.
+
+            μ = (1/m_h + 1/m_r)^-1
+
+                m_h: effective mass of the human body region (Table A.3).
+                m_r: effective mass of the robot as a function of the robot posture and motion.
+
+            m_r = M/2 + m_l
+
+                m_l: effective payload of the robot system, including tooling and workpiece.
+                M:   total mass of the moving parts of the robot.
+
+            v_rel = F / sqrt(μ*k) = p*A / sqrt(μ*k) -> v_rel_max = p_max*A / sqrt(μ*k)
+
+        """
+
+        """ Table A.2 - Biomechanical limits
+
+                Body Region     |  Specific Body Area  | Maximum Pressure | Maximum Force 
+                                                            [p, N/cm^2]          [N]
+
+            Skull and forehead    Middle of forehead            130              130
+            Skull and forehead    Temple                        110              130
+
+            Face                  Masticatory muscle            110              65
+
+            Neck                  Neck muscle                   140              150
+            Neck                  Seventh neck muscle           210              150
+
+            Back and shoulders    Shoulder joint                160              210
+            Back and shoulders    Fifth lumbar vertebra         210              210
+
+            Chest                 Sternum                       120              140
+            Chest                 Pectoral muscle               170              140
+
+            Abdomen               Abdominal muscle              140              110
+
+            Pelvis                Pelvic bone                   210              180
+
+            Upper arms            Deltoid muscle                190              150
+            Upper arms            Humerus                       220              150
+
+            Lower arms            Radial bone                   190              160
+            Lower arms            Forearm muscle                180              160
+            Lower arms            Arm nerve                     180              160
+
+            Hands and fingers     Forefinger pad                300              140
+            Hands and fingers     Forefinger end joint          280              140
+            Hands and fingers     Thenar eminence               200              140
+            Hands and fingers     Palm                          260              140
+            Hands and fingers     Back of the hand              200              140
+
+            Thighs and knees      Thigh muscle                  250              220
+            Thighs and knees      Kneecap                       220              220
+
+            Lower legs            Middle of shin                220              130
+            Lower legs            Calf muscle                   210              130
+
+        """
+
+        """ Table A.3 - Effective masses and spring constants for the body model
+
+            Body region         | Effective spring constant | Effective mass 
+                                          [K, N/mm]              [mH, kg]
+
+            Skull and forehead               150                    4,4
+            Face                             75                     4,4
+            Neck                             50                     1,2
+            Back and shoulders               35                     40
+            Chest                            25                     40
+            Abdomen                          10                     40
+            Pelvis                           25                     40
+            Upper arms and elbow joints      30                      3
+            Lower arms and wrist joints      40                      2
+            Hands and fingers                75                     0,6
+            Thighs and knees                 50                     75
+            Lower legs                       60                     75
+
+        """
+
+        """ PFL Parameters:
+
+            m_l = 1.0 kg               | effective payload of the robot system, including tooling and workpiece.
+            M   = sum(link 2 - link 6) | total mass of the moving parts of the robot.
+            m_h = 3.6 kg               | effective mass of the human body region (Table A.3) - Upper arms and elbow joints + Hands and fingers.
+            k   = 75 N/mm              | effective spring constant for specific body region (Table A.3) - max (Hands and fingers, Upper arms and elbow joints).
+            p   = 190 N/cm^2           | maximum contact pressure for specific body area (Table A.2) - min (Hands and fingers, Upper arms and elbow joints).
+            A   = 200 cm^2             | Contact area A is defined by the smaller of the surface areas of the robot or the operator.
+                                         In situations where the body contact surface area is smaller than robot contact surface area, such as the operator’s
+                                         hands or fingers, the body contact surface area shall be used. If contact between multiple body areas with  different
+                                         potential surface contact areas could occur, the value A that yields the lowest v_rel_max shall be used. - Hands and fingers.
+        """
+
+        # Define PFL Parameters
+        m_l, M, m_h, k, p, A = 1.0, np.sum(self.robot_mass[1:]), 3.6, 75, 190, 200
+
+        # Effective Mass of the Robot
+        m_r = M/2 + m_l
+
+        # Reduced Mass of the Two-Body System
+        μ = (1/m_h + 1/m_r)**-1
+
+        # Compute Relative Speed Between the Robot and the Human Body Region
+        v_rel_max = p*A / np.sqrt(μ*k)
+
+        return v_rel_max
 
     def compute_safety(self, desired_joint_velocity:np.ndarray,  joint_states:JointState, human_point:Vector3, human_vel:Vector3)  -> np.ndarray:
 
@@ -152,19 +259,26 @@ class SafetyController():
         if self.complete_debug: print(colored('SSM - Velocity Limit: ', 'green'), ssm_limit, '\n')
         elif self.debug: print(colored('SSM - Velocity Limit: ', 'green'), ssm_limit)
 
+        # Compute PFL ISO/TS 15066 Velocity Limit
+        pfl_limit = self.compute_PFL_vel_lim(P_H, P_R, hr_versor, human_vel)
+        if self.complete_debug: print(colored('PFL - Velocity Limit: ', 'green'), pfl_limit, '\n')
+        elif self.debug: print(colored('PFL - Velocity Limit: ', 'green'), pfl_limit)
+
+        # Compute Velocity Limit (Maximum Between SSM and PFL ISO/TS 15066 Velocity Limits)
+        # FIX: vel_limit = max(ssm_limit, pfl_limit)
+        vel_limit = ssm_limit
+
         # Compute Robot Projected Desired Velocity
         x_dot: np.ndarray = self.robot.Jacobian(np.array(joint_states.position)) @ np.array(desired_joint_velocity)
-        Vr = np.array([x_dot[0], x_dot[1], x_dot[2]]) @ np.array([hr_versor.x, hr_versor.y, hr_versor.z])
+        Vr = np.dot(np.array([x_dot[0], x_dot[1], x_dot[2]]), np.array([hr_versor.x, hr_versor.y, hr_versor.z]))
         if self.complete_debug: print(colored('Robot Desired Velocity: ', 'green'), x_dot)
         if self.complete_debug: print(colored('Robot Projected Desired Velocity: ', 'green'), Vr, '\n')
 
-        # Compute Scaling Factor (Alpha = V_max / Vr)
-        scaling_factor =  ssm_limit / Vr
+        # Compute Scaling Factor
+        scaling_factor = 1 if Vr <= 0 else min(vel_limit / Vr, 1)
         if self.debug or self.complete_debug: print(colored('Scaling Factor: ', 'green'), scaling_factor, '\n')
         if self.debug or self.complete_debug: print('-'*100, '\n')
-        # if 0 < scaling_factor < 1: print(colored('Scaling Factor: ', 'green'), scaling_factor, '\n')
 
-        import os
         print('-'*100, '\n')
         print(colored('Human Point: ', 'green'), f'{P_H.x} {P_H.y} {P_H.z}')
         print(colored('Robot Point: ', 'green'), f'{P_R.x} {P_R.y} {P_R.z}')
@@ -176,7 +290,5 @@ class SafetyController():
         print('-'*100, '\n')
         os.system('clear')
 
-        # Compute Scaled Joint Velocity (alpha < 0 -> robot moving away from human)
-        if scaling_factor >= 1: return desired_joint_velocity
-        elif 0 < scaling_factor < 1: return desired_joint_velocity * scaling_factor
-        elif scaling_factor <= 0: return desired_joint_velocity
+        # Return Scaled Joint Velocity
+        return scaling_factor * desired_joint_velocity, scaling_factor