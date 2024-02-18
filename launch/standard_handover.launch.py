ae08a109fa9f3aefdc67b4c79b48aa639df46a24
davideferrari95 <davideferr@unimore.it>
2024-02-18 19:43:32 +0100

    Minor

--- launch/standard_handover.launch.py
+++ launch/standard_handover.launch.py
@@ -1,5 +1,4 @@
 import os
-from typing import List
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
@@ -11,9 +10,12 @@ def create_standard_handover_node(context):
 
     # Python Node - Parameters
     standard_handover_parameters = {
+        'use_feedback_velocity': LaunchConfiguration('use_feedback_velocity'),
+        'admittance_weight': LaunchConfiguration('admittance_weight'),
         'complete_debug': LaunchConfiguration('complete_debug'),
         'debug': LaunchConfiguration('debug'),
         'sim': LaunchConfiguration('sim'),
+        'human_radius': LaunchConfiguration('human_radius'),
         'robot': LaunchConfiguration('robot'),
     }
 
@@ -40,11 +42,17 @@ def generate_launch_description():
     launch_description = LaunchDescription()
 
     # Arguments
+    use_feedback_velocity_arg = DeclareLaunchArgument('use_feedback_velocity', default_value='false')
+    admittance_weight_arg = DeclareLaunchArgument('admittance_weight', default_value='0.2')
     complete_debug_arg = DeclareLaunchArgument('complete_debug', default_value='false')
+    human_radius_arg = DeclareLaunchArgument('human_radius', default_value='0.2')
     debug_arg = DeclareLaunchArgument('debug', default_value='false')
     robot_arg = DeclareLaunchArgument('robot', default_value='ur5e')
     sim_arg = DeclareLaunchArgument('sim', default_value='false')
+    launch_description.add_action(use_feedback_velocity_arg)
+    launch_description.add_action(admittance_weight_arg)
     launch_description.add_action(complete_debug_arg)
+    launch_description.add_action(human_radius_arg)
     launch_description.add_action(debug_arg)
     launch_description.add_action(robot_arg)
     launch_description.add_action(sim_arg)
